309660bee26b0c97f6a0aea909cbf1099a8bed0e
Ramon Geronimo <rgero215@gmail.com>
2019-03-15 13:44:51 -0400

    add collection view

--- MLB Game Situation/HomeController.swift
+++ MLB Game Situation/HomeController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
     }
 
 
